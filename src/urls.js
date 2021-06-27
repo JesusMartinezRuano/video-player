7853065c708128b9bab945f97b6574fc61385125
JesusMartinezRuano <jesus.martinez.ruano@gmail.com>
2021-06-27 20:50:09 +0000

    pull

--- src/urls.js
+++ src/urls.js
@@ -1,5 +1,5 @@
 export const urls = [
   {
-    "url1" : "http://18.168.248.67/webdav/UHDSpain_SDR.m3u8",
-    "url2" : "http://dwstream4-lh.akamaihd.net:80/i/dwstream4_live@131329/index_1_av-b.m3u8"
+    "url1" : "https://hlsliveamdgl0-lh.akamaihd.net/i/hlslive_1@586402/master.m3u88",
+    "url2" : "http://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master_1660.m3u8"
   }]
